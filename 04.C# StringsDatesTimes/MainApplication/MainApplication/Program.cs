1aa9f1aa03622ec8b2fb0080e6f7ad28779c27c1
Emanuel Caprariu <manu.caprariu.brediceanu@gmail.com>
2022-03-14 17:50:08 +0200

    04.Strings with dates and times

--- 04.C# StringsDatesTimes/MainApplication/MainApplication/Program.cs
+++ 04.C# StringsDatesTimes/MainApplication/MainApplication/Program.cs
@@ -24,24 +24,35 @@ var hasFirstName = fullName.Contains("Emanuel");
 //A = B = 0
 //A > B = 1
 var compare = string.Compare("a", "A",StringComparison.InvariantCultureIgnoreCase); //nu tine cont de cultura deci sunt egale
+var compare2 = string.Compare("alabala", "abama", StringComparison.InvariantCultureIgnoreCase); // 1
+
 
 //TimeSpan
 var timeSpan = new TimeSpan(6, 0, 0);
 var currentDate = DateTime.Now - timeSpan; // scazi 6 ore din data curenta
-currentDate = DateTime.Now.Date.Add(timeSpan); // adaugi 6 ore
+Console.WriteLine($"Current date - timespan: {currentDate}");
+currentDate = DateTime.Now.Date.Add(timeSpan); // setezi din acea zi 6 ore
+Console.WriteLine($"Current date with timespan : {currentDate}");
 
 //DateTime
-
 var dateTimeUTC = DateTime.UtcNow;
-var dateTimeOffSet = DateTimeOffset.Now; // te ajuta pentru regiuni offsetul asta raportat la UTC
+var dateTimeOffSet = DateTimeOffset.Now; // te ajuta pentru regiuni offsetul raportat la UTC
+
+Console.WriteLine($"Date time utc: {dateTimeUTC}\nDate time offset: {dateTimeOffSet}");
 
 //Convert DateTime
-var date = new DateTime(2002, 03, 14);
-var formattedDate = date.ToString("yyyy-MM-dd");
+var date = new DateTime(DateTime.Now.Year - 23, 03, 14);
+var formattedDate = date.ToString("dd.MM.yyyy");
 var secondFormattedDate = date.ToString("dddd MMMM");
 var thirdFormattedDate = date.ToString("dddd MMMM", CultureInfo.CreateSpecificCulture("ro-RO"));
+Console.WriteLine($"Formatted Date: {formattedDate}");
 
-var numberCulture = 10.ToString("C", new CultureInfo("en-BW"));
+var numberCulture = 10.ToString("C", new CultureInfo("ro-RO"));
 
+
+//NumberFormatInfo
+NumberFormatInfo numberFormatInfo = new NumberFormatInfo();
+numberFormatInfo.NumberGroupSeparator = " ";
+Console.WriteLine(12134.214124.ToString("N1",numberFormatInfo)); // 12 134.213124
 
 Console.WriteLine(numberCulture);
